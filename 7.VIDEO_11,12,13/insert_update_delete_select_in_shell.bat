c25b87dd4a59bd496a0042de72d265914f27cb91
José Gómez <ing.jggomeza@gmail.com>
2020-03-07 16:16:16 -0430

    07/03/2020 Clase 11-12-13

--- 7.VIDEO_11,12,13/insert_update_delete_select_in_shell.bat
+++ 7.VIDEO_11,12,13/insert_update_delete_select_in_shell.bat
@@ -1,4 +1,3 @@
-
 E:\projects_django\7.VIDEO_11,12>python manage.py shell
 Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license" for more information.
